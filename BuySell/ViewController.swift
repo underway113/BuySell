b87ba84ed2f5f9a8a8b16384b703f4384b5afbab
Jeremy Adam <underway113@gmail.com>
2019-06-17 21:29:01 +0700

    Initial Commit

---
+++ BuySell/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BuySell
+//
+//  Created by Jeremy Adam on 17/06/19.
+//  Copyright © 2019 Underway. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
